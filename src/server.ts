16ee97236a11c94419c1b91e686947a9df9f9ec8
Long <longiter04@gmail.com>
2026-03-10 10:50:02 +0700

    implement security middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import subjectsRouter from "./routes/subjects";
 import cors from "cors";
+import SecurityMiddleware from "./middleware/security";
 const app = express();
 const PORT = 8000;
 
@@ -11,6 +12,8 @@ app.use(cors({
 }))
 
 app.use(express.json());
+
+app.use(SecurityMiddleware)
 
 app.get('/', (_req, res) => {
   res.send('Hello World!');
